68df75d3b925b545333cd423fcc3eb57f8f8480c
Nur Shalahuddin <shotozuro@gmail.com>
2019-06-12 12:32:53 +0700

    wip velocity function

--- main.m
+++ main.m
@@ -34,6 +34,9 @@ c1=2*pi;
 omg_r=fb/fp;
 c2=2*pi*omg_r;
 
+m = 0.5;
+We = 0.5;
+
 for i = 1 : N+1
   x(i) = (i - 1) * dx;  
 end
@@ -45,6 +48,15 @@ end
 N = 4;
 nt = 5;
 w = zeros(N+1, nt)
+
+% koefisien R
+delta = 0.2;
+etal = 4 * delta;
+epsi = 0.2;
+z = 0.52;
+
+R = (1 + epsi * z) * (1 - 64 / 10 * etal * ((11/32 * (z-sigma)) - (47/48 * ((z-sigma) ^ 2)) + ((z - sigma)^3) - (1/3 * ((z-sigma) ^4)) ))
+
 for i = 1 : N+1
   for j = 1 : nT
     if t(j) == 0
@@ -55,8 +67,11 @@ for i = 1 : N+1
       printf("masuk 3");
       w(1,j) = w(2,j)
      else
-      w(i, j-1) = i * j
-##      w(i,j-1) = w(i,j) + dt / alpha * (B1 * (1 + e * cos(c1 * t(j))) + B2 * (cos(c2 * t(j) + P)) )
+      dw = (w(i+1, j) - w(i-1,j)) / 2 * dx;
+      ddw = (w(i+1,j) - 2 * w(i,j) + w(i-1, j)) / dx ^ 2;
+      w(i,j-1) = w(i,j) + (dt / alpha * (B1 * (1 + e * cos(c1 * t(j))) + B2 * (cos(c2 * t(j) + P)) ))...
+        + (1 / (x(i) * R^2) * (m + ((1-m) * (1 + (We / R * abs(dw)) ^ (n-1) ) ^ (-1)) ) * dw...
+        + (ddw / (R ^ 2) * (m + 1))
      end
    endfor
 endfor
